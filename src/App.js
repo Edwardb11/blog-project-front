d4676f21026037beac742138903b4666dc644513
Edward Brito Diaz <edwardbrito11@hotmail.com>
2022-02-15 21:28:46 -0400

    add creating the app componnetn with react router dom home page

--- src/App.js
+++ src/App.js
@@ -1,3 +1,14 @@
+import React from "react";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 export default function App() {
-  return <h1 className="text-3xl font-bold underline text-center">Hello world!</h1>;
+  return (
+    <Router>
+      <div className="max-w-screen-md mx-auto pt-20">
+        <Routes>
+          <Route path="/" element={<Home />}></Route>{" "}
+        </Routes>{" "}
+      </div>{" "}
+    </Router>
+  );
 }
